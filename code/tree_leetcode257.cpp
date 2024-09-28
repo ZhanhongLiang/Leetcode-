6c87a5b995cc3e12a718e86d0e7b038b164a3c7c
jean <cw252128385@gmail.com>
2024-09-28 11:44:16 +0800

    leetcode 404

--- code/tree_leetcode257.cpp
+++ code/tree_leetcode257.cpp
@@ -2,7 +2,7 @@
  * @Author: Jean_Leung
  * @Date: 2024-09-27 19:38:35
  * @LastEditors: Jean_Leung
- * @LastEditTime: 2024-09-27 20:23:38
+ * @LastEditTime: 2024-09-28 10:56:50
  * @FilePath: \code\tree_leetcode257.cpp
  * @Description:
  *
@@ -36,51 +36,45 @@ struct TreeNode {
 
 class Solution {
   public:
-    // 递归函数应该返回什么??
-    // 根据root left right的单层结构画图来看
-    // 返回应该是void,因为我们需要得到一个vector<string>
-    // 那么必定是通过参数的形式添加到vector里面，并不是通过返回的形式
-    // 递归终止条件: 当空指针就返回
-    // 单层递归:
+    // 第二天回顾算法---因为是比较难的算法
+    // 这道题可以用后序遍历吗?答案是很难，因为是正的路径,如果是用后序递归遍历,那么返回的路径就是倒过来到
+    // 不符合规则的倒叙路径
+    // 所以这道求性质的题目适合前序遍历
+    // 法1:使用前序递归遍历
+    // 确定入参和返参
+    // 因为是vector<string>的返场,我们不太方便能使用string的返回参数
     void getPath(TreeNode *root, vector<int> &path, vector<string> &result) {
-        // // 终止条件
-        // 这么写很麻烦，如果这么写,当到了空结点时候
-        // 我们返回
-        // if (root == NULL) {
-        //     return;
-        // }
-        // 需要添加每个结点的路径到vector里面
-        path.push_back(root->val);
+        // 前序遍历
+        // vector<int>是记录每个路径访问
+        // 方便使用回溯
+        path.push_back(root->val); // 访问路径添加当前结点
+        // 终止条件，为什么不能使用空结点作为依据，因为当判空的时候,path里面也添加了空结点,不好进行回溯
+        // 所以需要用叶子结点作为判空
         if (root->left == NULL && root->right == NULL) {
-            string s_path;
-            // 当前结点是叶子结点,那么直接在回溯矩阵中遍历
+            // 如果左右叶子结点都是空
+            // 将path结点添加到result中
+            string s;
             for (int i = 0; i < path.size() - 1; i++) {
-                s_path += to_string(path[i]);
-                s_path += "->";
+                s += to_string(path[i]);
+                s += "->";
             }
-            s_path += to_string(path[path.size() - 1]);
-            result.push_back(s_path);
+            s += to_string(path[path.size() - 1]);
+            result.push_back(s);
             return;
         }
-        // 如果不是终止状态
+        // 如果非叶子结点,继续遍历
         if (root->left) {
             getPath(root->left, path, result);
-            // 需要回溯
-            path.pop_back();
+            path.pop_back(); // 回溯
         }
         if (root->right) {
             getPath(root->right, path, result);
-            path.pop_back();
+            path.pop_back(); // 回溯
         }
     }
-
-    // 还是求解二叉树的性质
-    // 用后序还是前序??用后序的话会产生 子节点->父节点的结果
-    // 并不符合题意
-    // 所有我们需要前序遍历,这个时候我们肯定可以用前序递归遍历和前序迭代遍历
     vector<string> binaryTreePaths(TreeNode *root) {
-        vector<string> result;
         vector<int> path;
+        vector<string> result;
         if (root == NULL) {
             return result;
         }
